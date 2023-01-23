52dd7dd74050bbf01e65fbfac272ac4b381a9ced
charles madhuku <dastilesforever@gmail.com>
2023-01-23 11:07:05 +0200

    whatapp clone commit

---
+++ components/Navbar.js
@@ -0,0 +1,18 @@
+import Link from "next/link"
+
+
+function Nav() {
+    return (
+        <nav className="bg-whatsApp_blue">
+            <div className="bg-whatsApp_blue">
+                <h1>WhatsApp</h1>
+                <div>
+                    <Link href={'/'}>
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav
